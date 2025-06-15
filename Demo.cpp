3ea4f4e2e5e798b4eb005d8d39e6581ed6e33003
Phillip Magachi <phillip.magachi@strathmore.edu>
2025-06-15 19:11:06 +0300

    father's day

---
+++ Demo.cpp
@@ -0,0 +1,77 @@
+<!DOCTYPE html>  <!-- document type declaration (DTD)-->
+<!--root element (it contains all the others )-->
+<<html>
+     <head> <!-- metadata (description of what the html page has)-->
+       <!-- Oven - You'll love it -- >
+     <title> Tandoori - Utaipenda </title>
+     <!--Oven  - You'll love it -->
+     <meta name="keywords" content="barking,cakes,oven baked"
+     <meta name="description" content="irresistible goodness"  
+     link rel="shortcut icon" href="assets/Favicon.jpg" type="image/jpg">
+     <style>
+      /an element selector .. all headings ones on the page/
+      h1{
+      color:  brown; /* color is the property*/
+      text align: center; /text-align is the property/
+
+      }
+      /*an element selector */
+      body{
+        background-color: #5C7F88;;
+      }
+      /an element selector/
+      h2{
+        text-decoration: underline;
+        /* an id selsector*/
+        #banner{
+          border-radius: 20px;
+          width: 60%;
+        }
+        /* a class selector*/
+        .bold{
+          font-weight: bold; /font-weight is the property/
+        }
+        .italics{
+          font-style: italic; /font-style is the property/
+        }
+
+      }
+    </style>
+  </head>  
+    <body> <!-- the page content (what the user sees)-->
+        <!--a heading (header)-->
+        <!--inline css-->
+        <h1 style="text decoration: underline;"></h1>
+        <h1> Tandoori <i> Utaipenda </i> </h1>
+        <!-- a banner image-->
+         <img id="banner" src="assets/banner.jpg" alt= "banner">
+        <!-- some content -->
+         <h2>Welcome to Oven Baked Sweetness</h2>
+         <h3>Where every treat is baked with love</h3>
+         <p class="bold italics">
+             At Oven Baked Sweetness, we specialize in creating iresisteble oven-fresh delights that warms the hearts
+             and satisfy every sweet tooth
+             </p>
+             <!--2D bbt2 bbt undergradute sces strathmore-->  
+             <p>
+                Each treat is made from premium ingredients, traditional techniques, and a touch of creativity. Whether youré craving comfort classics or seasonal specialities, our kitchen is always brimming with something delightful. 
+                <h2>
+                  Our Signature Specialties:</h2>
+                             <ul>
+                <li>Soft-baked cookies in unique and classic flavour</li>
+                <li>Flanky, fruit-filled pies made with reeal butter crusts</li>
+                <li>Decadent brownies and bars with rich, fudgy centers</li>
+                <li>Small-batch artisan cakes for every occasion </li>
+            </ul>
+            <h2>Today's offers </h2>
+            <!-- an orderd list-->    
+              <o1>
+                <li>Brownie </li> <Li>Coconut muffin</Li>
+              </o1>
+            
+        <!-- some more content at the bottom (footer)-->
+         <a href=""> "https://www.instagram.com/the_bakers_club/" </a>
+              target="_blank"
+
+    </body>
+</html>
